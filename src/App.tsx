55dbcb110273f78358354bb3d931dd981ea26119
Meshahan <meshahan@gmail.com>
2026-01-05 14:23:35 +0500

    Fix White Screen: Use HashRouter and relative paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
 import Menu from "./pages/Menu";
@@ -32,7 +32,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <HashRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/menu" element={<Menu />} />
@@ -50,7 +50,7 @@ const App = () => (
                 <Route path="*" element={<NotFound />} />
               </Routes>
               <AIChatbot />
-            </BrowserRouter>
+            </HashRouter>
           </TooltipProvider>
         </CartProvider>
       </ThemeProvider>
